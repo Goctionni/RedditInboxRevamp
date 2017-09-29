82ca91436c7922d1d5cde827f5432bc1308f8c8d
Goctionni <goctionni@gmail.com>
2017-09-29 22:34:52 +0200

    Clean Slate

--- js/content-all/messageListener.js
+++
@@ -1,6 +0,0 @@
-function rirMessageListener(request, sender, sendResponse){
-    console.log(request, sender, sendResponse);
-    alert('Message received');
-}
-
-chrome.runtime.onMessage.addListener(rirMessageListener);
